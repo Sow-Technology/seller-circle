01837a70d18ed97c1abf08c3f1faaa9b4b677c41
Kanish Kumar <kanishkumar11@outlook.in>
2024-06-19 20:39:48 +0530

    minor updates

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,11 +10,11 @@ const Page = () => {
   return (
     <>
       <Navbar />
-      <div className="w-full  bg-white  bg-grid-black/[0.2] relative flex items-center justify-center ">
+      <div className="w-full  bg-white  bg-grid-black/[0.05] relative flex items-center justify-center ">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center  bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-        <div className="max-w-7xl mx-auto my-20 ">
+        <div className="max-w-7xl mx-auto my-20 z-[39]">
           <motion.h3
             initial={{ y: 100, opacity: 0 }}
             whileInView={{ y: 0, opacity: 100 }}
