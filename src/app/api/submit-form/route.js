9c2d0d738eca9edb8eed82d8cbc61e5399b9a5b2
Kanish Kumar <kanishkumar11@outlook.in>
2024-08-11 14:09:26 +0530

    minor fix2

--- src/app/api/submit-form/route.js
+++ src/app/api/submit-form/route.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-import Form from '../../models/Form';
 import axios from 'axios';
+import Form from '@/models/Form';
 
 const connectToDatabase = async () => {
   if (mongoose.connection.readyState === 1) return;
