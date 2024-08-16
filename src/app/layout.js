eb23a364c9b04f08e0dd634c1a53eb5108d3bb56
Kanish Kumar <kanishkumar11@outlook.in>
2024-08-16 19:35:44 +0530

    minor error fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 const nunito = Nunito_Sans({ subsets: ["latin"], variable: "--nsans" });
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Seller Circle",
@@ -13,9 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>
-        {children} <Toaster position={"top-center"} />
-      </body>
+      <Suspense>
+        <body className={nunito.className}>
+          {children} <Toaster position={"top-center"} />
+        </body>
+      </Suspense>
     </html>
   );
 }
